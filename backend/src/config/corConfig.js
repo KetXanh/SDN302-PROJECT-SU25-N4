3f81d913fb6ec0f9a318b370346fa8ba6ef0dcde
KetXanh <169112526+KetXanh@users.noreply.github.com>
2025-08-25 22:13:22 +0700

    He

--- backend/src/config/corConfig.js
+++ backend/src/config/corConfig.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 require('dotenv').config();
 const applyConfig = (app) => {
     app.use(cors({
-        origin: ["http://localhost:5173", "http://192.168.139.1:5173"],
+        origin: ["*"],
         credentials: true,
     }));
 };
